e2098d7653e4bbdace41e5b76fb34c55719984bd
Eric Askari <eric.askari@weare.fi>
2022-01-26 11:08:41 +0200

    test

--- playground-atlassian-pipeline/node/README.md
+++
@@ -1,3 +0,0 @@
-
-
-        docker build -t node-test . && docker run --rm -e VERSION=v1.0.0 node-test
--- playground-atlassian-pipeline/node/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-mkdir -p /usr/local/lib/nodejs
-tar -xJf ./node-v16.13.2-linux-x64.tar.xz -C /usr/local/lib/nodejs
-node --version
-npm --version
