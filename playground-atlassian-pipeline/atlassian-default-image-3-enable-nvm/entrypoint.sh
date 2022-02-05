e5229a444c47f01b699ec2521c20fa02af6545c8
Eric Askari <eric.askari@weare.fi>
2022-02-05 14:02:23 +0200

    ran migration reset

--- playground-atlassian-pipeline/atlassian-default-image-3-enable-nvm/entrypoint.sh
+++ playground-atlassian-pipeline/atlassian-default-image-3-enable-nvm/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-node --version
-npm --version
+#node --version
+#npm --version
+#
+#source "$NVM_DIR"/nvm.sh
+#nvm install lts/gallium
+#
+#node --version
+#npm --version
 
-source "$NVM_DIR"/nvm.sh
-nvm install lts/gallium
-
-node --version
-npm --version
+printenv
